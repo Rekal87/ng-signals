027f93d7e7e5ae00143c6bb33aa64b83bcba37a0
Nnamdi <nnamdi.b@gmail.com>
2024-01-14 23:24:45 +0100

    new file

---
+++ src/app/declarative/declarative.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-declarative',
+  standalone: true,
+  imports: [],
+  templateUrl: './declarative.component.html',
+  styleUrl: './declarative.component.css'
+})
+export class DeclarativeComponent {
+
+}
